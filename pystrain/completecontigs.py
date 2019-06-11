8111518c702489522cda785989d05b52f90e1b57
Rhys Newell <rhys.newell@uqconnect.edu.au>
2019-06-12 07:39:01 +1000

    Small shift around

--- pystrain/completecontigs.py
+++ pystrain/completecontigs.py
@@ -64,7 +64,7 @@ def buildContigs(assemblyCoords, binCoords, simple=True, outputDirectory='./', m
         if assemblyCoords.source[contig] == 'r':
             for entry in assemblyCoords.generate(contig):
                 if entry.q_tag in binCoords.query.keys() and entry.r_tag not in binCoords.reference.keys():
-                    contig_fraction = assemblyCoords.reference[entry.r_tag][entry.s1_start:entry.s1_end+1]
+
                     if simple:
                         if entry.percent_id >= min_id and entry.s1_len >= min_length:
                             new_ref_contigs[entry.r_tag] = assemblyCoords.reference[entry.r_tag]
@@ -77,6 +77,7 @@ def buildContigs(assemblyCoords, binCoords, simple=True, outputDirectory='./', m
                                                       fragmentposition = [entry.s1_start, entry.s1_end],
                                                       percentid = entry.percent_id))
                             except KeyError:
+                                contig_fraction = assemblyCoords.reference[entry.r_tag][entry.s1_start:entry.s1_end + 1]
                                 new_ref_contigs[entry.r_tag] = sequence.Sequence(contig_fraction,
                                                                                  name=entry.r_tag,
                                                                                  fragmentposition = [entry.s1_start, entry.s1_end],
@@ -84,7 +85,7 @@ def buildContigs(assemblyCoords, binCoords, simple=True, outputDirectory='./', m
         if assemblyCoords.source[contig] == 'q':
             for entry in assemblyCoords.generate(contig):
                 if entry.q_tag not in binCoords.query.keys() and entry.r_tag in binCoords.reference.keys():
-                    contig_fraction = assemblyCoords.query[entry.q_tag][entry.s2_start:entry.s2_end+1]
+
                     if simple:
                         if entry.percent_id >= min_id and entry.s2_len >= min_length:
                             new_query_contigs[entry.q_tag] = assemblyCoords.query[entry.q_tag]
@@ -97,6 +98,7 @@ def buildContigs(assemblyCoords, binCoords, simple=True, outputDirectory='./', m
                                                       fragmentposition=[entry.s2_start, entry.s2_end],
                                                       percentid=entry.percent_id))
                             except KeyError:
+                                contig_fraction = assemblyCoords.query[entry.q_tag][entry.s2_start:entry.s2_end + 1]
                                 new_query_contigs[entry.q_tag] = sequence.Sequence(contig_fraction,
                                                                                    name=entry.q_tag,
                                                                                    fragmentposition = [entry.s2_start, entry.s2_end],
@@ -127,7 +129,7 @@ def twoSampleBuildContigs(assemblyCoordsFile, binCoordsDirectory, outputDirector
     try:
         os.mkdir(outputDirectory + "/referencebins")
     except FileExistsError:
-        print("Overwriting previous query bins")
+        print("Overwriting previous reference bins")
 
     for file in binCoords:
         if file.endswith(".coords"):
