e420033ccada2a37824a3d8b7b78fd0889237d06
Rhys Newell <rhys.newell@uqconnect.edu.au>
2019-06-18 13:01:40 +1000

    Moved over to pyfaidx for faster fasta reading, new contig building method, input is now assembly coords and the bin.fna files

--- pystrain/completecontigs.py
+++ pystrain/completecontigs.py
@@ -1,37 +1,101 @@
 #!/usr/bin/env python3
 import pystrain.coords as coords
 import pystrain.sequence as sequence
+import pyfaidx
 import sys, os, glob
 import importlib
 
+def connectFragment(oldFragment, newFragment):
+    """
+
+    :param newFragment: A sequence from the same contig as the original sequence from sequence class in pyfaidx
+    :return: A combined sequence
+    """
+
+    # Set percentIdentity attribute placehodler in case it doesn't exist yet
+    try:
+        oldFragment.percentIdentity
+    except AttributeError:
+        oldFragment.percentIdentity = 100
+
+    try:
+        newFragment.percentIdentity
+    except AttributeError:
+        newFragment.percentIdentity = 100
+
+    if oldFragment.name.strip() == newFragment.name.strip():
+        # newFragment is before current fragment
+        if newFragment.end < oldFragment.start:
+            ambiguousN = ''.join(['N']*(oldFragment.start-newFragment.end-1))
+            oldFragment.seq = newFragment.seq + ambiguousN + oldFragment.seq
+            oldFragment.start = newFragment.start
+        # newFragment is after current fragment
+        elif oldFragment.end < newFragment.start:
+            ambiguousN = ''.join(['N']*(newFragment.start-oldFragment.end-1))
+            oldFragment.seq = oldFragment.seq + ambiguousN + newFragment.seq
+            oldFragment.end = newFragment.end
+        # newFragment is inside current sequence, so only replace ambiguous regions
+        elif oldFragment.start < newFragment.start and oldFragment.end > newFragment.end:
+            start = newFragment.start - oldFragment.start
+            end = newFragment.end - oldFragment.start
+            subSeq = oldFragment.seq[start:end+1]
+            replaceSeq = ''
+            replacement_cnt = 0
+
+            # Replace any ambiguous characters in fragment region
+            if len(subSeq) != len(newFragment.seq):
+                print("differing sequence lengths?", len(subSeq), len(newFragment.seq))
+            for (i, sym) in enumerate(subSeq):
+                if sym == 'N':
+                    replaceSeq += newFragment.seq[i]
+                    replacement_cnt += 1
+                else:
+
+                    if oldFragment.percentIdentity >= newFragment.percentIdentity:
+                        replaceSeq += sym
+                    else:
+                        replaceSeq += newFragment.seq[i]
+                        replacement_cnt += 1
+
+            oldFragment.percentIdentity = (oldFragment.percentIdentity+newFragment.percentIdentity)/2
+            # print("".join(oldFragment.seq[:start]), replaceSeq, "".join(oldFragment.seq[end+1:]))
+            oldFragment.seq = "".join(oldFragment.seq[:start]) + replaceSeq + "".join(oldFragment.seq[end+1:])
+        return oldFragment
+    else:
+        print("Sequences are from different contigs:")
+        print(oldFragment.name, newFragment.name)
+        return oldFragment
+
 def tests():
-    seq_a = sequence.Sequence('ATGCGT', name = 'contig1', fragmentposition=[1, 6])
-    print(seq_a, seq_a.fragmentStart, seq_a.fragmentEnd)
-    seq_b = sequence.Sequence('GCGAGT', name = 'contig1', fragmentposition=[13, 18])
-    seq_a.connectFragment(seq_b)
+    seq_a = pyfaidx.Sequence('contig1', 'ATGCGT', start=1, end=6)
+    print(seq_a, seq_a.start, seq_a.end)
+    seq_b = pyfaidx.Sequence('contig1', 'GCGAGT', start=13, end=18)
+    seq_a = connectFragment(seq_a, seq_b)
     # Connect fragment after original
-    print(seq_a, seq_a.fragmentStart, seq_a.fragmentEnd)
+    print(seq_a, seq_a.start, seq_a.end)
     # Wrong contig
-    seq_c = sequence.Sequence('CCCCCC', name = 'cotnig1', fragmentposition=[7, 12])
-    seq_a.connectFragment(seq_c)
-    seq_c = sequence.Sequence('CCCCCC', name = 'contig1', fragmentposition=[7, 12])
-    seq_a.connectFragment(seq_c)
+    seq_c = pyfaidx.Sequence('different_contig1', 'CCCCCC', start=7, end=12)
+    seq_a = connectFragment(seq_a, seq_c)
+    seq_c = pyfaidx.Sequence('contig1', 'CCCCCC', start=7, end=12)
+    seq_a = connectFragment(seq_a, seq_c)
     # Substitute fragment in centre
-    print(seq_a, seq_a.fragmentStart, seq_a.fragmentEnd)
-    seq_a = sequence.Sequence('ATGCGT', name='contig1', fragmentposition=[1, 6])
-    seq_b = sequence.Sequence('GCGAGT', name='contig1', fragmentposition=[13, 18], percentid=97.1)
-    seq_b.connectFragment(seq_a)
+    print(seq_a, seq_a.start, seq_a.end)
+    seq_a = pyfaidx.Sequence('contig1', 'ATGCGT', start=1, end=6)
+    seq_b = pyfaidx.Sequence('contig1', 'GCGAGT', start=13, end=18)
+    seq_b.percentIdentity = 97.1
+    seq_b = connectFragment(seq_b, seq_a)
     # Connect fragment before original
-    print(seq_b, seq_b.fragmentStart, seq_b.fragmentEnd, seq_b.percentIdentity)
-    seq_c = sequence.Sequence('CCC', name='contig1', fragmentposition=[5, 7], percentid=97.2)
-    seq_b.connectFragment(seq_c)
-    print(seq_b, seq_b.fragmentStart, seq_b.fragmentEnd, seq_b.percentIdentity)
-    seq_c = sequence.Sequence('TTT', name='contig1', fragmentposition=[10, 12])
-    seq_b.connectFragment(seq_c)
-    print(seq_b, seq_b.fragmentStart, seq_b.fragmentEnd)
+    print(seq_b, seq_b.start, seq_b.end, seq_b.percentIdentity)
+    seq_c = pyfaidx.Sequence('contig1', 'CCC', start=5, end=7)
+    seq_c.percentIdentity = 97.2
+    seq_b = connectFragment(seq_b, seq_c)
+    print(seq_b, seq_b.start, seq_b.end, seq_b.percentIdentity)
+    seq_c = pyfaidx.Sequence('contig1', 'AATTT', start=8, end=12)
+    seq_b = connectFragment(seq_b, seq_c)
+    print(seq_b, seq_b.start, seq_b.end, len(seq_b))
 
 
-# tests()
+tests()
 # testcoords = coords.readCoordFile("tests/out.coords")
 # ref = sequence.readFastaFile("tests/73.20100900_E3D.15.fna")
 # query = sequence.readFastaFile("tests/r2.parent.d77.174_unicyc.fna")
@@ -50,7 +114,7 @@ def tests():
 # bin_coords = coords.readCoordFile("tests/bin.filter.coords")
 
 
-def buildContigs(assemblyCoords, binCoords, simple=True, outputDirectory='./', min_length = 2000, min_id = 85):
+def buildContigs(assemblyCoords, bin, simple=True, outputDirectory='./', min_length = 2000, min_id = 85):
     """
 
     :param assemblyCoords: The alignment coords produced by nucmer when aligning two assemblies contigs together
@@ -58,57 +122,53 @@ def buildContigs(assemblyCoords, binCoords, simple=True, outputDirectory='./', m
     :return: Two new bin .fna files with any additional contigs that were found to be present in both samples and
     binned in one species
     """
-    new_ref_contigs = {}
-    new_query_contigs = {}
-    for contig in assemblyCoords.contigs:
-        if assemblyCoords.source[contig] == 'r':
-            for entry in assemblyCoords.generate(contig):
-                if entry.q_tag in binCoords.query.keys() and entry.r_tag not in binCoords.reference.keys():
-
+    new_contigs = {}
+    connections = {}
+    for contig in bin.index.keys():
+        for query_contig in assemblyCoords.generate(contig, source='q'):
+            for ref_contig in assemblyCoords.generate(query_contig.r_tag, source='r'):
+                if ref_contig.q_tag not in bin.index.keys():
                     if simple:
-                        if entry.percent_id >= min_id and entry.s1_len >= min_length:
-                            new_ref_contigs[entry.r_tag] = assemblyCoords.reference[entry.r_tag]
+                        if ref_contig.percent_id >= min_id and ref_contig.s2_len >= min_length:
+                            new_contigs[ref_contig.q_tag] = assemblyCoords.query.fetch(ref_contig.q_tag, 1,
+                                                                                       assemblyCoords.query.index[ref_contig.q_tag].rlen)
                     else:
-                        if entry.percent_id >= min_id and entry.s1_len >= min_length:
+                        if ref_contig.percent_id >= min_id and ref_contig.s2_len >= min_length:
                             try:
-                                new_ref_contigs[entry.r_tag].connectFragment(
-                                    sequence.Sequence(contig_fraction,
-                                                      name=entry.r_tag,
-                                                      fragmentposition = [entry.s1_start, entry.s1_end],
-                                                      percentid = entry.percent_id))
+                                contig_fraction = assemblyCoords.query.fetch(ref_contig.q_tag,
+                                                                             ref_contig.s2_start, ref_contig.s2_end)
+                                contig_fraction.percentIdentity = ref_contig.percent_id
+                                new_contigs[ref_contig.q_tag] = connectFragment(new_contigs[ref_contig.q_tag],
+                                                                                contig_fraction)
                             except KeyError:
-                                contig_fraction = assemblyCoords.reference[entry.r_tag][entry.s1_start:entry.s1_end + 1]
-                                new_ref_contigs[entry.r_tag] = sequence.Sequence(contig_fraction,
-                                                                                 name=entry.r_tag,
-                                                                                 fragmentposition = [entry.s1_start, entry.s1_end],
-                                                                                 percentid = entry.percent_id)
-        if assemblyCoords.source[contig] == 'q':
-            for entry in assemblyCoords.generate(contig):
-                if entry.q_tag not in binCoords.query.keys() and entry.r_tag in binCoords.reference.keys():
+                                contig_fraction = assemblyCoords.query.fetch(ref_contig.q_tag,
+                                                                             ref_contig.s2_start, ref_contig.s2_end)
+                                contig_fraction.percentIdentity = ref_contig.percent_id
+                                new_contigs[ref_contig.q_tag] = contig_fraction
+        # if assemblyCoords.source[contig] == 'q':
+        #     for entry in assemblyCoords.generate(contig):
+        #         if entry.q_tag not in bin.query.keys() and entry.r_tag in bin.reference.keys():
+        #
+        #             if simple:
+        #                 if entry.percent_id >= min_id and entry.s2_len >= min_length:
+        #                     new_query_contigs[entry.q_tag] = assemblyCoords.query[entry.q_tag]
+        #             else:
+        #                 if entry.percent_id >= min_id and entry.s2_len >= min_length:
+        #                     try:
+        #                         new_query_contigs[entry.q_tag].connectFragment(
+        #                             sequence.Sequence(contig_fraction,
+        #                                               name=entry.q_tag,
+        #                                               fragmentposition=[entry.s2_start, entry.s2_end],
+        #                                               percentid=entry.percent_id))
+        #                     except KeyError:
+        #                         contig_fraction = assemblyCoords.query[entry.q_tag][entry.s2_start:entry.s2_end + 1]
+        #                         new_query_contigs[entry.q_tag] = sequence.Sequence(contig_fraction,
+        #                                                                            name=entry.q_tag,
+        #                                                                            fragmentposition = [entry.s2_start, entry.s2_end],
+        #                                                                            percentid=entry.percent_id)
 
-                    if simple:
-                        if entry.percent_id >= min_id and entry.s2_len >= min_length:
-                            new_query_contigs[entry.q_tag] = assemblyCoords.query[entry.q_tag]
-                    else:
-                        if entry.percent_id >= min_id and entry.s2_len >= min_length:
-                            try:
-                                new_query_contigs[entry.q_tag].connectFragment(
-                                    sequence.Sequence(contig_fraction,
-                                                      name=entry.q_tag,
-                                                      fragmentposition=[entry.s2_start, entry.s2_end],
-                                                      percentid=entry.percent_id))
-                            except KeyError:
-                                contig_fraction = assemblyCoords.query[entry.q_tag][entry.s2_start:entry.s2_end + 1]
-                                new_query_contigs[entry.q_tag] = sequence.Sequence(contig_fraction,
-                                                                                   name=entry.q_tag,
-                                                                                   fragmentposition = [entry.s2_start, entry.s2_end],
-                                                                                   percentid=entry.percent_id)
 
-    sequence.writeFastaFile(outputDirectory+"/referencebins/"+"new_"+binCoords.reference_name,
-                            list(binCoords.reference.values())+list(new_ref_contigs.values()))
-
-    sequence.writeFastaFile(outputDirectory+"/querybins/"+"new_"+binCoords.query_name,
-                            list(binCoords.query.values())+list(new_query_contigs.values()))
+    return new_contigs
 
 
 def twoSampleBuildContigs(assemblyCoordsFile, binCoordsDirectory, outputDirectory, minLength, minMatch, simple):
@@ -131,8 +191,21 @@ def twoSampleBuildContigs(assemblyCoordsFile, binCoordsDirectory, outputDirector
     for file in binCoords:
         if file.endswith(".fna") or file.endswith(".fa"):
             print("Working on: ", file)
-            bin = sequence.readFastaFile(file)
-            buildContigs(assembly_coords, bin, simple, outputDirectory, minLength, minMatch)
+            bin = pyfaidx.Faidx(file)
+            new_contigs = buildContigs(assembly_coords, bin, simple, outputDirectory, minLength, minMatch)
+            with open(outputDirectory + "/new_" + file.split('/')[-1], 'w') as fh:
+
+                for contig in bin.index.keys():
+                    seq = bin.fetch(contig, 1, bin.index[contig].rlen)
+                    fasta = ">" + seq.name + '\n'
+                    fasta += seq.seq + '\n'
+                    fh.write(fasta)
+                for contig in new_contigs.keys():
+                    fasta = '>' + contig + '\n'
+                    fasta += new_contigs[contig].seq
+                    fasta += '\n'
+                    fh.write(fasta)
+
     print("done!")
 
 
--- pystrain/coords.py
+++ pystrain/coords.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import string, sys, re, math, os, array
 import numpy
+import pyfaidx
 import pystrain.ival as ival
 import pystrain.sequence as sequence
 
@@ -23,95 +24,105 @@ class nucmerCoords(object):
     q_tag = None
 
     def __init__(self, entry):
-        self.s1_start = int(entry[0])-1
-        self.s1_end = int(entry[1])-1
+        self.s1_start = int(entry[0])
+        self.s1_end = int(entry[1])
         if self.s1_start > self.s1_end:
             self.s1_strand = '-'
-            self.s1_start = int(entry[1])-1
-            self.s1_end = int(entry[0])-1
+            self.s1_start = int(entry[1])
+            self.s1_end = int(entry[0])
         else:
             self.s1_strand = '+'
         self.s1_len = int(entry[4])
-        self.s2_start = int(entry[2])-1
-        self.s2_end = int(entry[3])-1
+        self.s2_start = int(entry[2])
+        self.s2_end = int(entry[3])
         if self.s2_start > self.s2_end:
             self.s2_strand = '-'
-            self.s2_start = int(entry[3])-1
-            self.s2_end = int(entry[2])-1
+            self.s2_start = int(entry[3])
+            self.s2_end = int(entry[2])
         else:
             self.s2_strand = '+'
         self.s2_len = int(entry[5])
         self.percent_id = float(entry[6])
         self.r_len = int(entry[7])
         self.r_cov = float(entry[9])
-        self.r_tag = str(entry[11])+"_r"
+        self.r_tag = str(entry[11])
         self.q_len = int(entry[8])
         self.q_cov = float(entry[10])
-        self.q_tag = str(entry[12])+"_q"
+        self.q_tag = str(entry[12])
 
     def __str__(self):
         """ Defines what should be printed when the print statement is used on a Sequence instance """
         entry = format("%d\t%d\t%s\t%d\t%d\t%s\t%d\t%d\t%.3f\t%d\t%d\t%.3f\t%.3f\t%s\t%s" %
                        (self.s1_start, self.s1_end, self.s1_strand, self.s2_start, self.s2_end, self.s2_strand,
                         self.s1_len, self.s2_len, self.percent_id, self.r_len, self.q_len, self.r_cov, self.q_cov,
-                        self.r_tag.strip('_r'), self.q_tag.strip('_q')))
+                        self.r_tag.strip(), self.q_tag.strip()))
         return entry
 
 
 class coordFile():
 
     def __init__(self, entries, reference_path, query_path):
-        self.source = dict()
-        self.contigs = dict()
-        # self.q_contigs = dict()
+        # self.source = dict()
+        # self.contigs = dict()
+        self.r_contigs = dict()
+        self.q_contigs = dict()
         self.reference_name = reference_path.split('/')[-1]
-        self.reference = dict()
-        reference = sequence.fastaFile(reference_path)
-        for refSeq in reference:
-            self.reference[refSeq.name+"_r"] = refSeq
+        self.reference = pyfaidx.Faidx(reference_path)
+
         self.query_name = query_path.split('/')[-1]
-        self.query = dict()
-        query = sequence.fastaFile(query_path)
-        for queSeq in query:
-            self.query[queSeq.name+"_q"] = queSeq
+        self.query = pyfaidx.Faidx(query_path)
+
 
         for entry in entries:
             # check if the ref contig has been seen before
-            ref_tree = self.contigs.get(entry.r_tag)
+            ref_tree = self.r_contigs.get(entry.r_tag)
             if not ref_tree:
                 ref_tree = ival.IntervalTree()
-                self.contigs[entry.r_tag] = ref_tree
-                self.source[entry.r_tag] = 'r'
+                self.r_contigs[entry.r_tag] = ref_tree
+                # self.source[entry.r_tag] = 'r'
             # put the entry in the reference interval tree for the appropriate contig
             iv = ival.Interval(entry.s1_start, entry.s1_end)
             ref_tree.put(iv, entry)
 
             # check if the query contig has been seen before
-            query_tree = self.contigs.get(entry.q_tag)
+            query_tree = self.q_contigs.get(entry.q_tag)
             if not query_tree:
                 query_tree = ival.IntervalTree()
-                self.contigs[entry.q_tag] = query_tree
-                self.source[entry.q_tag] = 'q'
+                self.q_contigs[entry.q_tag] = query_tree
+                # self.source[entry.q_tag] = 'q'
             # put the entry in the interval tree for the appropriate contig
             iv = ival.Interval(entry.s2_start, entry.s2_end)
             query_tree.put(iv, entry)
 
     def __len__(self):
         n = 0
-        for c in self.contigs:
-            n += len(self.contigs[c])
+        for c in self.r_contigs:
+            n += len(self.r_contigs[c])
         return n
 
-    def generate(self, contig):
-        mytree = self.contigs.get(contig)
-        if mytree is not None:
-            for e in mytree:
-                for entry in e.values:
-                    yield entry
+    def generate(self, contig, source='r'):
+        """
+
+        :param contig: The contig name
+        :param source: Whether you want to reference or query contig. Contig names can be shared across assemblies
+        :return: A coordinate for that contig
+        """
+        if source.lower().startswith('r'):
+            mytree = self.r_contigs.get(contig)
+            if mytree is not None:
+                for e in mytree:
+                    for entry in e.values:
+                        yield entry
+        elif source.lower().startswith('q'):
+            mytree = self.q_contigs.get(contig)
+            if mytree is not None:
+                for e in mytree:
+                    for entry in e.values:
+                        yield entry
 
     def __iter__(self):
         self.contigqueue = ival.Stack()
-        for c in sorted(self.contigs.keys())[::-1]:
+        for c in sorted(self.r_contigs.keys())[::-1]:
             self.contigqueue.push(self.generate(c))
         self.current = self.contigqueue.pop()
         return self
@@ -129,66 +140,12 @@ class coordFile():
 
     def __contains__(self, item):
         if isinstance(item, nucmerCoords):
-            tree = self.contigs.get(item.r_tag)
+            tree = self.r_contigs.get(item.r_tag)
             if tree == None: return False
             else: return ival.Interval(item.s1_start, item.s1_end) in tree
         else:
             return False
 
-    def getOverlap(self, item):
-        """
-        :param item: a nucmerCoord entry
-        :return: an array of lists of overlaps, first item is reference overlaps, second is query overlaps
-        """
-        if isinstance(item, nucmerCoords):
-            overlaps = {}
-            tree = self.contigs.get(item.r_tag)
-            if tree == None:
-                overlaps[item.r_tag] = [None]
-            else:
-                iv = ival.Interval(item.s1_start, item.s1_end)
-                res = tree.isectall(iv)
-                ret = []
-                for r in res:
-                    ret.extend(r.values)
-                overlaps[item.r_tag] = ret
-
-            tree = self.contigs.get(item.q_tag)
-            if tree == None:
-                overlaps[item.q_tag] = [None]
-            else:
-                iv = ival.Interval(item.s2_start, item.s2_end)
-                res = tree.isectall(iv)
-                ret = []
-                for r in res:
-                    ret.extend(r.values)
-                overlaps[item.q_tag] = ret
-
-            return overlaps
-        else:
-            return {'reference':[None], 'query':[None]}
-
-    def getClosest(self, item):
-        if isinstance(item, nucmerCoords):
-            tree = self.contigs.get(item.r_tag)
-            if tree == None: return None
-            else:
-                iv = ival.Interval(item.s1_start, item.s1_end)
-                node = tree.closest(iv)
-                if node != None: return node.values
-                else: return None
-        else: return None
-
-    def getOneOfClosest(self, item):
-        all = self.getClosest(item)
-        if all == None: return None
-        else: return next(iter(all))
-
-    def getOneOfOverlap(self, item):
-        all = self.getOverlap(item)
-        if all == None: return None
-        elif len(all) == 0: return None
-        else: return next(iter(all))
 
 def readCoordFile(filename):
     coordlist = []
@@ -220,5 +177,5 @@ if __name__=='__main__':
     print(filename)
     coords = readCoordFile(filename)
     print(coords)
-    for coord in coords:
+    for coord in coords.generate('query_contig1', source='q'):
         print(coord)
